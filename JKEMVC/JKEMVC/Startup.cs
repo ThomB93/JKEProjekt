ac88dc17733a60da04efa06c9223745bb7346eaf
thomb <thomb@DESKTOP-SA7T54T>
2016-11-25 16:10:17 +0100

    Tilføjede projekt

---
+++ JKEMVC/JKEMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JKEMVC.Startup))]
+namespace JKEMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

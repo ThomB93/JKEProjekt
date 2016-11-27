9ac3215f024365c7f0681536c7600178b71118e4
Thomas <thomb@hotmail.dk>
2016-11-27 15:59:50 +0100

    Tilføjet form til at sende mail i contact view

---
+++ JKEMVC/JKEMVC/Models/EmailFormModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JKEMVC.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace JKEMVC.Models
+{
+    public class EmailFormModel
+    {
+        [Required, Display(Name = "Your name")]
+        public string FromName { get; set; }
+        [Required, Display(Name = "Your email"), EmailAddress]
+        public string FromEmail { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}

8594c8132773ed9b27087e4eac30cd131870f74e
Thomas <thomb@hotmail.dk>
2016-11-27 15:05:28 +0100

    TIlføjet html helper class til visning af aktivt menupunkt (virker ikke endnu)

---
+++ JKEMVC/JKEMVC/HTMLHelperClasses/MenuHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace JKEMVC.HTMLHelperClasses
+{
+    public static class MenuHelper
+    {
+        /// <summary>
+        /// Extension method for <see cref="HtmlHelper"/> to support highlighting the active tab on the default MVC menu
+        /// </summary>
+        /// <param name="htmlHelper"></param>
+        /// <param name="linkText">The text to display in the link</param>
+        /// <param name="actionName">Link target action name</param>
+        /// <param name="controllerName">Link target controller name</param>
+        /// <param name="activeClass">The CSS class to apply to the link if active</param>
+        /// <param name="checkAction">If true, checks the current action name to determine if the menu item is 'active', otherwise only the controller name is matched</param>
+        /// <returns></returns>
+        public static MvcHtmlString MenuLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName, string activeClass, bool checkAction)
+        {
+            string currentAction = htmlHelper.ViewContext.RouteData.GetRequiredString("action");
+            string currentController = htmlHelper.ViewContext.RouteData.GetRequiredString("controller");
+
+            if (string.Compare(controllerName, currentController, StringComparison.OrdinalIgnoreCase) == 0 && ((!checkAction) || string.Compare(actionName, currentAction, StringComparison.OrdinalIgnoreCase) == 0))
+            {
+                return htmlHelper.ActionLink(linkText, actionName, controllerName, null, new { @class = activeClass });
+            }
+
+            return htmlHelper.ActionLink(linkText, actionName, controllerName);
+
+        }
+        public static MvcHtmlString MenuLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName, string activeClass)
+        {
+            return MenuLink(htmlHelper, linkText, actionName, controllerName, activeClass, true);
+        }
+    }
+}

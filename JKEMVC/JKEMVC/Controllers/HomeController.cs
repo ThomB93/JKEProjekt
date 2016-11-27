b874608c495d20a5ece5e089a992591013c1ba5f
Casper <casp.mad@gmail.com>
2016-11-27 14:59:52 +0100

    Kan sende en mail til mig fra Contact.

--- JKEMVC/JKEMVC/Controllers/HomeController.cs
+++ JKEMVC/JKEMVC/Controllers/HomeController.cs
@@ -3,11 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JKEMVC.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace JKEMVC.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("mvctest@cool.dk"));  // replace with valid value 
+                message.From = new MailAddress("casp.mad@gmail.com");  // replace with valid value
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "casp.mad@gmail.com",  // replace with valid value
+                        Password = "Frants123"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Sent()
+        {
+            return View();
+        }
+
         public ActionResult Index()
         {
             return View();
